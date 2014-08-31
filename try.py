770d1f1c7042f2dc4f347116c7aa9bab3d3b558f
Rajkumar Kumawat <rajkumarsikar.86@gmail.com>
2014-08-31 16:06:30 -0700

    Try.py Commented

--- try.py
+++ try.py
@@ -14,17 +14,17 @@
 #input("You're a winner face!!")
 #
 #
-class Hero:
-	def __init__(self,name):
-		self.name=name
-		self.health=100
-	def eat(self,food):
-		if(food=='apple'):
-			self.health -=100
-		elif(food=='ham'):
-			self.health +=50
-Bob=Hero("Bob")
-print (Bob.name)
-print (Bob.health)
-Bob.eat('apple')
-print (Bob.health)
+#class Hero:
+#	def __init__(self,name):
+#		self.name=name
+#		self.health=100
+#	def eat(self,food):
+#		if(food=='apple'):
+#			self.health -=100
+#		elif(food=='ham'):
+#			self.health +=50
+#Bob=Hero("Bob")
+#print (Bob.name)
+#print (Bob.health)
+#Bob.eat('apple')
+#print (Bob.health)
